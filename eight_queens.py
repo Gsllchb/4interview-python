fb84eb48b173b24a8d153cec9cca0ac2d40077a5
Gsllchb <Gsllchb@gmail.com>
2018-03-22 12:57:37 +0800

    update

---
+++ eight_queens.py
@@ -0,0 +1,32 @@
+import copy
+import math
+
+N = 8
+counter = 0
+
+
+def arrange_eight_queens():
+    chess = [['O' for j in range(N)] for i in range(N)]
+    set_queen(chess, N, set(), set(), set())
+
+
+def set_queen(chess, remain: int, cols: set, diagonal1: set, diagonal2: set) -> None:
+    global counter
+    if remain == 0:
+        print('=' * 20)
+        print(counter)
+        counter += 1
+        for row in chess:
+            print(''.join(row))
+        return
+
+    y = remain - 1
+    for x in range(N):
+        if x not in cols and (x - y) not in diagonal1 and (N - 1 - x - y) not in diagonal2:
+            new_chess = copy.deepcopy(chess)
+            new_chess[y][x] = 'X'
+            set_queen(new_chess, remain - 1, cols | {x, }, diagonal1 | {x - y, }, diagonal2 | {N - 1 - x - y, })
+
+
+if __name__ == '__main__':
+    arrange_eight_queens()
